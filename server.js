1a7202c12c23ae7acdff43fdcee8a1f33d1729c3
Jose <SkyFall7k@gmail.com>
2025-05-16 13:30:16 -0400

    Configurada conexión SSL/TLS para la base de datos PostgreSQL en Render

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432, // Asegurarse de que el puerto sea un número
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // API para obtener la configuración global
